36ea0c0ba6b1c028d41f93212b74882cd65c984b
Jake Spann <jake323@comcast.net>
2018-07-27 10:54:58 -0500

    Initial Commit

---
+++ Spedometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spedometer
+//
+//  Created by Jake Spann on 7/27/18.
+//  Copyright © 2018 JakeSpann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
